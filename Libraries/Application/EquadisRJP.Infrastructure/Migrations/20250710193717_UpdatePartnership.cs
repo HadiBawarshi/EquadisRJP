529806e8015b9e145fc48fa0f1d9c6704014c8b9
Hadi Bawarshi <81911956+HadiBawarshi@users.noreply.github.com>
2025-07-10 22:42:52 +0300

    chore: adding mig to update the partnership in the db

---
+++ Libraries/Application/EquadisRJP.Infrastructure/Migrations/20250710193717_UpdatePartnership.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquadisRJP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePartnership : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Supplier");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Supplier");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Partnership",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Partnership",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Partnership");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Partnership");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Supplier",
+                type: "nvarchar(511)",
+                maxLength: 511,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Supplier",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+    }
+}
