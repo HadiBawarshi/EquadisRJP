d76a952b5ae64d33cdea489fd9fa4acdcede0f34
Hadi Bawarshi <81911956+HadiBawarshi@users.noreply.github.com>
2025-07-13 16:59:22 +0300

    chore: updating exceptions in registration and create token

--- Libraries/Identity/EquadisRJP.IdentityAuth.Public/Responses/RegisterResponseDto.cs
+++ Libraries/Identity/EquadisRJP.IdentityAuth.Public/Responses/RegisterResponseDto.cs
@@ -6,13 +6,15 @@
         {
         }
 
-        public RegisterResponseDto(bool success, string? userId)
+        public RegisterResponseDto(bool success, string? userId, string? message = "")
         {
             Success = success;
             UserId = userId;
+            Message = message;
         }
 
         public bool Success { get; set; }
         public string? UserId { get; set; }
+        public string? Message { get; set; }
     }
 }
